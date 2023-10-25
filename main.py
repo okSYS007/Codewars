fbb5a51e8faf3d25d09fbe9cfe3d2d1d8aadc1ec
okSYS <ViacheslavDan803@gmail.com>
2023-10-25 18:18:55 +0300

    add new kata

--- main.py
+++
@@ -1,7 +0,0 @@
-def pig_it(text):
-    
-   return " ".join(x[1:] + x[0] + "ay" if x.isalnum() else x for x in text.split())
-
-# print(pig_it('Pig latin is cool')) #'igPay atinlay siay oolcay')
-# print(pig_it('This is my string')) #'hisTay siay ymay tringsay')
-print(pig_it('O tempora o mores !'))  # Oay emporatay oay oresmay !
