6f2a86dc736f4764bf4641ad1df2828cbd55f1c2
okSYS <ViacheslavDan803@gmail.com>
2023-10-25 18:49:41 +0300

    add more kata

--- Stop gninnipS My sdroW!.js
+++ Stop gninnipS My sdroW!.js
@@ -7,10 +7,14 @@
 // spinWords( "This is a test") => returns "This is a test"
 // spinWords( "This is another test" )=> returns "This is rehtona test"
 
-//первый символ chatAt это 0 символ, остальные символы slice полсе 0 символа
 function spinWords(string) {
-  return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
+  return string.split(' ').map(word => word.length >= 5 ? word.split('').reverse().join('') : word).join(' ');
 }
 
 console.log(spinWords("Welcome")) //"emocleW"
 console.log(spinWords("Hey fellow warriors")) // "Hey wollef sroirraw");
+console.log(spinWords("This is a test")) //, "This is a test");
+console.log(spinWords("This is another test")) //, "This is rehtona test");
+console.log(spinWords("You are almost to the last test")) //,"You are tsomla to the last test"
+console.log(spinWords("Just kidding there is still one more")) //,"Just gniddik ereht is llits one more"
+console.log(spinWords("Seriously this is the last one")) //,"ylsuoireS this is the last one"
