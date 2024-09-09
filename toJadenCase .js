435a3263d83a4988df1bf41f9f825c790bb03964
okSYS <ViacheslavDan803@gmail.com>
2024-09-09 08:40:23 +0300

    new

--- toJadenCase .js
+++
@@ -1,8 +0,0 @@
-
-//первый символ chatAt это 0 символ, остальные символы slice полсе 0 символа
-function spinWords(string) {
-  return string.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(" ");
-}
-
-console.log(spinWords("Welcome")) //"emocleW"
-console.log(spinWords("Hey fellow warriors")) // "Hey wollef sroirraw");
