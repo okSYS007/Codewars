50d53929265631614b0b3ca7ba3cb404ba75d073
okSYS <ViacheslavDan803@gmail.com>
2026-01-03 17:50:42 +0100

    end

--- Codewars/Краткость IV - Индекс элемента в массиве.py
+++ Codewars/Краткость IV - Индекс элемента в массиве.py
@@ -20,21 +20,3 @@ print(find(array, 3)) # 1
 print(find(array, 2)) # 0
 print(find(array, 8)) # 0
 
-# array = [2,3,5,7,11]
-#         test.assert_equals(find(array, 5), 2)
-#         test.assert_equals(find(array, 11), 4)
-#         test.assert_equals(find(array, 3), 1)
-#         test.assert_equals(find(array, 2), 0)
-#         test.assert_equals(find(array, 7), 3)
-#         test.assert_equals(find(array, 1), 'Not found')
-#         test.assert_equals(find(array, 8), 'Not found')
-
-#         array = [True, 'Hello World', False, 'Lorem Ipsum', 6, pi];
-#         test.assert_equals(find(array, 'Hello World'), 1)
-#         test.assert_equals(find(array, 'lorem ipsum'), 'Not found')
-#         test.assert_equals(find(array, 'Lorem Ipsum'), 3)
-#         test.assert_equals(find(array, False), 2)
-#         test.assert_equals(find(array, True), 0)
-#         test.assert_equals(find(array, pi), 5)
-#         test.assert_equals(find(array, 3.14), 'Not found')
-#         test.assert_equals(find(array, 6), 4)
