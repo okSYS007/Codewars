5743e1d652c932225641ae0d59d7a61660a9a0a9
okSYS <ViacheslavDan803@gmail.com>
2026-01-05 10:48:10 +0100

    duck task

---
+++ Codewars/Duck Duck Goose.py
@@ -0,0 +1,12 @@
+
+# Given an array of Player objects and a position (first position is 1), 
+# return the name of the chosen Player.
+
+# name is a property of Player objects, e.g Player.name
+
+# duck_duck_goose([a, b, c, d], 1) should return a.name
+# duck_duck_goose([a, b, c, d], 5) should return a.name
+# duck_duck_goose([a, b, c, d], 4) should return d.name
+
+def duck_duck_goose(players, goose):
+    pass
