9ccd420c01bcc419aa394326e20e18915d11754d
okSYS <ViacheslavDan803@gmail.com>
2026-01-05 10:51:31 +0100

    add

--- Codewars/Duck Duck Goose.py
+++ Codewars/Duck Duck Goose.py
@@ -1,12 +1,31 @@
 
-# Given an array of Player objects and a position (first position is 1), 
-# return the name of the chosen Player.
+# Цель игры «Утка, утка, гусь» — ходить по кругу , постукивая по голове каждого игрока, пока не будет выбран один из них.
 
-# name is a property of Player objects, e.g Player.name
-
+# Имея массив объектов Player и позицию (первая позиция равна 1), верните идентификатор nameвыбранного игрока.
+# name— это свойство Playerобъектов, например,Player.name
 # duck_duck_goose([a, b, c, d], 1) should return a.name
 # duck_duck_goose([a, b, c, d], 5) should return a.name
 # duck_duck_goose([a, b, c, d], 4) should return d.name
 
 def duck_duck_goose(players, goose):
     pass
+
+class Player:
+    name: str
+
+players = [Player(name) for name in "abcdcefghz"]
+
+print(duck_duck_goose(players, 1)) # "a"
+print(duck_duck_goose(players, 3)) # "c"
+print(duck_duck_goose(players, 10)) # "z"  
+print(duck_duck_goose(players, 20)) # "z"   
+# test.assert_equals(duck_duck_goose(players, 1),  "a")
+# test.assert_equals(duck_duck_goose(players, 3),  "c")
+# test.assert_equals(duck_duck_goose(players, 10), "z")
+# test.assert_equals(duck_duck_goose(players, 20), "z")
+# test.assert_equals(duck_duck_goose(players, 30), "z")
+# test.assert_equals(duck_duck_goose(players, 18), "g")
+# test.assert_equals(duck_duck_goose(players, 28), "g")
+# test.assert_equals(duck_duck_goose(players, 12), "b")
+# test.assert_equals(duck_duck_goose(players, 2),  "b")
+# test.assert_equals(duck_duck_goose(players, 7),  "f")
