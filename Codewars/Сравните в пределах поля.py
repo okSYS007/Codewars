6e1605cdf649bb842d65304ddf2c8b51b01ae5eb
okSYS <ViacheslavDan803@gmail.com>
2026-01-06 18:40:25 +0100

    new task

---
+++ Codewars/Сравните в пределах поля.py
@@ -0,0 +1,14 @@
+
+def close_compare(a, b, margin):
+    pass
+
+
+# test.assert_equals(close_compare(4, 5), -1)
+# test.assert_equals(close_compare(5, 5), 0)
+# test.assert_equals(close_compare(6, 5), 1)
+
+# test.assert_equals(close_compare(2, 5, 3), 0)
+# test.assert_equals(close_compare(5, 5, 3), 0)
+# test.assert_equals(close_compare(8, 5, 3), 0)
+# test.assert_equals(close_compare(8.1, 5, 3), 1)
+# test.assert_equals(close_compare(1.99, 5, 3), -1)
