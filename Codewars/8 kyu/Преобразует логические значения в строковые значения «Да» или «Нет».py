615ed691dd931e84d857e85fdc771b9338fcc8eb
okSYS <ViacheslavDan803@gmail.com>
2026-04-22 13:24:16 +0200

    end

---
+++ Codewars/8 kyu/Преобразует логические значения в строковые значения «Да» или «Нет».py
@@ -0,0 +1,4 @@
+# Завершите метод, который принимает логическое значение и возвращает "Yes"строку для true, или "No"строку для false.
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
