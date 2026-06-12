102485cc092b3ae53f241f2b9a93fe414cbc9ee9
Ok SYS <ViacheslavDan803@gmail.com>
2026-06-12 11:47:10 +0200

    end

--- Codewars/6 kyu/TTT17Разделение четных и нечетных чисел.py
+++ Codewars/6 kyu/TTT17Разделение четных и нечетных чисел.py
@@ -17,15 +17,34 @@
 # splitOddAndEven(123456)  ===  [1,2,3,4,5,6]
 
 def split_odd_and_even(n):
-    pass
+    digits = str(n)
+    parts = []
+    start = 0
+    current_parity = int(digits[0]) % 2
 
-        #    test.assert_equals(split_odd_and_even(123), [1,2,3])
-        # test.assert_equals(split_odd_and_even(223),  [22,3])
-        # test.assert_equals(split_odd_and_even(111),  [111])
-        # test.assert_equals(split_odd_and_even(13579),  [13579])
-        # test.assert_equals(split_odd_and_even(2468642), [2468642])
-        # test.assert_equals(split_odd_and_even(135246),  [135,246])
-        # test.assert_equals(split_odd_and_even(123456),  [1,2,3,4,5,6])
-        # test.assert_equals(split_odd_and_even(8123456),  [8,1,2,3,4,5,6])
-        # test.assert_equals(split_odd_and_even(82123456),  [82,1,2,3,4,5,6])
-        # test.assert_equals(split_odd_and_even(88123456),  [88,1,2,3,4,5,6])
+    for index, digit in enumerate(digits[1:], start=1):
+        parity = int(digit) % 2
+        if parity != current_parity:
+            parts.append(int(digits[start:index]))
+            start = index
+            current_parity = parity
+
+    parts.append(int(digits[start:]))
+    return parts
+
+# --- local tests ---
+if __name__ == "__main__":
+    from scripts.kata_check import run_tests
+
+    run_tests(split_odd_and_even, [
+        ((123,), [1, 2, 3]),
+        ((223,), [22, 3]),
+        ((111,), [111]),
+        ((13579,), [13579]),
+        ((2468642,), [2468642]),
+        ((135246,), [135, 246]),
+        ((123456,), [1, 2, 3, 4, 5, 6]),
+        ((8123456,), [8, 1, 2, 3, 4, 5, 6]),
+        ((82123456,), [82, 1, 2, 3, 4, 5, 6]),
+        ((88123456,), [88, 1, 2, 3, 4, 5, 6]),
+    ])
