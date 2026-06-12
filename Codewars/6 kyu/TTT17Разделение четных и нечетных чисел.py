600012e7a11b1389b2f45a6c38fc1cdc797110ef
Ok SYS <ViacheslavDan803@gmail.com>
2026-06-12 11:45:46 +0200

    kata: update TTT17Разделение четных и нечетных чисел

---
+++ Codewars/6 kyu/TTT17Разделение четных и нечетных чисел.py
@@ -0,0 +1,31 @@
+# Задача
+# Полная функция splitOddAndEvenпринимает число n(n>0) и возвращает массив, содержащий последовательные части нечетных или четных цифр.
+
+# Не беспокойтесь о цифре 0, она не появится ;-)
+
+# Примеры
+# splitOddAndEven(123)  ===  [1,2,3]
+
+# splitOddAndEven(223)  ===  [22,3]
+
+# splitOddAndEven(111)  ===  [111]
+
+# splitOddAndEven(13579)  ===  [13579]
+
+# splitOddAndEven(135246)  ===  [135,246]
+
+# splitOddAndEven(123456)  ===  [1,2,3,4,5,6]
+
+def split_odd_and_even(n):
+    pass
+
+        #    test.assert_equals(split_odd_and_even(123), [1,2,3])
+        # test.assert_equals(split_odd_and_even(223),  [22,3])
+        # test.assert_equals(split_odd_and_even(111),  [111])
+        # test.assert_equals(split_odd_and_even(13579),  [13579])
+        # test.assert_equals(split_odd_and_even(2468642), [2468642])
+        # test.assert_equals(split_odd_and_even(135246),  [135,246])
+        # test.assert_equals(split_odd_and_even(123456),  [1,2,3,4,5,6])
+        # test.assert_equals(split_odd_and_even(8123456),  [8,1,2,3,4,5,6])
+        # test.assert_equals(split_odd_and_even(82123456),  [82,1,2,3,4,5,6])
+        # test.assert_equals(split_odd_and_even(88123456),  [88,1,2,3,4,5,6])
