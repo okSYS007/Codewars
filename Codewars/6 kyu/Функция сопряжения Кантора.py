94b9ac8cac413987cb0a36643cae77cb818f7a4e
Ok SYS <ViacheslavDan803@gmail.com>
2026-06-14 11:46:29 +0200

    end

--- Codewars/6 kyu/Функция сопряжения Кантора.py
+++ Codewars/6 kyu/Функция сопряжения Кантора.py
@@ -18,5 +18,39 @@
 
 # Вывод: строка - nthвыражение последовательности - 'a/b'где aи - bцелые числа.
 
+from math import isqrt
+
+
 def cantor(n : int) -> str:
-    pass
+    diagonal = (isqrt(8 * n + 1) - 1) // 2
+    if diagonal * (diagonal + 1) // 2 < n:
+        diagonal += 1
+
+    previous_count = diagonal * (diagonal - 1) // 2
+    position = n - previous_count
+
+    if diagonal % 2 == 0:
+        numerator = position
+        denominator = diagonal + 1 - position
+    else:
+        numerator = diagonal + 1 - position
+        denominator = position
+
+    return f"{numerator}/{denominator}"
+
+
+if __name__ == "__main__":
+    from scripts.kata_check import run_tests
+
+    run_tests(cantor, [
+        ((1,), "1/1"),
+        ((2,), "1/2"),
+        ((3,), "2/1"),
+        ((4,), "3/1"),
+        ((5,), "2/2"),
+        ((6,), "1/3"),
+        ((7,), "1/4"),
+        ((10,), "4/1"),
+        ((15,), "1/5"),
+        ((268435455,), "22590/581"),
+    ])
